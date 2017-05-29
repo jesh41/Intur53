d3f5e7601d18861ac6744381b0ada2f9bf95538a
Ernesto Herrera <jesh40@hotmail.es>
2017-05-28 22:17:47 -0600

    'layout'

--- resources/views/reports/Index.blade.php
+++ resources/views/reports/Index.blade.php
@@ -2,19 +2,12 @@
 
 @section('content')
 <div class="row">
-            <div class="col-xs-12">
+            <div class="col-md-12">
               <div class="box box-info">
                 <div class="box-header">
                   <h3 class="box-title">REPORTES DEL SISTEMA</h3>
                   <div class="box-tools">
-                 <!--   <div class="input-group" style="width: 150px;">
-                      <input type="text" name="table_search" class="form-control input-sm pull-right" placeholder="Search">
-                      <div class="input-group-btn">
-                        <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
-                      </div>
-                    </div>
-                  </div>
-                </div> /.box-header -->
+               
                 <div class="box-body table-responsive no-padding">
                   <table class="table table-hover">
                    
