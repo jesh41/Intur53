db61019a1159faaaad84d2f36942ed275daa49f8
Ernesto Herrera <jesh40@hotmail.es>
2017-07-22 18:53:45 -0600

    test ide

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 
-        <div class="row">
+    <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-success">
                 <div class="panel-heading">Menu Home</div>
 
                 <div class="panel-body">
-  <p style="text-align: justify;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:14.0pt">Sistema de indicadores estad&iacute;stico en hoteles Aparta Hoteles y alojamientos tur&iacute;sticos (5 a 1 Estrellas) de Managua. Tiene como objetivo crear indicadores en tiempo oportuno sobre el impacto y desarrollo del sector hotelero en la econom&iacute;a, de esta manera se podr&aacute; emplear <span style="color:black">de manera eficaz planes o estrategias para el mejoramiento de sus programas y mejoras en la toma de decisiones.</span></span></span></span>
-</p>
-<p style="text-align: justify;"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:14.0pt"><span style="color:black">La recopilaci&oacute;n de los libros de registros en formato Excel tendr&aacute; un impacto positivo en las estad&iacute;sticas siendo de gran utilidad para la emisi&oacute;n de informes e insumo para el bolet&iacute;n anual de la Actividad Hotelera, extendiendo el proyecto a otras ciudades y rutas tur&iacute;sticas.</span></span></span></span>
-</p>
+                    <p style="text-align: justify;"><span style="font-size:11pt"><span
+                                    style="font-family:Calibri,sans-serif"><span style="font-size:14.0pt">Sistema de indicadores estad&iacute;stico en hoteles Aparta Hoteles y alojamientos tur&iacute;sticos (5 a 1 Estrellas) de Managua. Tiene como objetivo crear indicadores en tiempo oportuno sobre el impacto y desarrollo del sector hotelero en la econom&iacute;a, de esta manera se podr&aacute; emplear <span
+                                            style="color:black">de manera eficaz planes o estrategias para el mejoramiento de sus programas y mejoras en la toma de decisiones.</span></span></span></span>
+                    </p>
+                    <p style="text-align: justify;"><span style="font-size:11pt"><span
+                                    style="font-family:Calibri,sans-serif"><span style="font-size:14.0pt"><span
+                                            style="color:black">La recopilaci&oacute;n de los libros de registros en formato Excel tendr&aacute; un impacto positivo en las estad&iacute;sticas siendo de gran utilidad para la emisi&oacute;n de informes e insumo para el bolet&iacute;n anual de la Actividad Hotelera, extendiendo el proyecto a otras ciudades y rutas tur&iacute;sticas.</span></span></span></span>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 
-           
+
 
 
 @endsection
--- resources/views/reports/report1grafica.blade.php
+++ resources/views/reports/report1grafica.blade.php
@@ -36,7 +36,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">TEST</div>
+                <div class="panel-heading">PRUEBA CON IDE</div>
                 <div class="panel-body">
                     <div id="container"></div>
                 </div>
