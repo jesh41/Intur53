262de887e750ab61ae61ad93ff56526f7baf0e76
Ernesto Herrera <jesh40@hotmail.es>
2017-07-22 21:07:58 -0600

    formato

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use Auth;
 use Illuminate\Http\Request;
 use Excel;
@@ -19,21 +18,19 @@ use Illuminate\Support\Facades\Validator;
 use Caffeinated\Shinobi\Models\Role;
 use Caffeinated\Shinobi\Models\Permission;
 
-
 class ReportController extends Controller
 {
-      public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
 
-
-   public function index()
+    public function index()
     {
         return view('/reports/Index');
     }
 
- public function crearPDF($datos,$vistaurl,$tipo)
+    public function crearPDF($datos, $vistaurl, $tipo)
     {
 
         $data = $datos;
@@ -41,65 +38,68 @@ class ReportController extends Controller
         $view = \View::make($vistaurl, compact('data', 'date'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view);
-        
-        if($tipo==1){return $pdf->stream('reporte');}
-        if($tipo==2){return $pdf->download('reporte.pdf'); }
+
+        if ($tipo == 1) {
+            return $pdf->stream('reporte');
+        }
+        if ($tipo == 2) {
+            return $pdf->download('reporte.pdf');
+        }
     }
 
-    public function crear_reporte_porpais($tipo){
-      //falta capturar y mandar el parametro
-     $year=2017;
-     $vistaurl="/reports/report1";
-     $detalle=DB::select("call indicador1general(2017)");
-     return $this->crearPDF($detalle, $vistaurl,$tipo);
+    public function crear_reporte_porpais($tipo)
+    {
+        //falta capturar y mandar el parametro
+        $year = 2017;
+        $vistaurl = "/reports/report1";
+        $detalle = DB::select("call indicador1general(2017)");
+
+        return $this->crearPDF($detalle, $vistaurl, $tipo);
     }
 
-     public function crear_reporte_por_sexo($tipo){
-      //falta capturar y mandar el parametro
-     $year=2017;
-     $vistaurl="/reports/report2";
-     $detalle=DB::select("call indicador2(2017)");
-     return $this->crearPDF($detalle, $vistaurl,$tipo);
+    public function crear_reporte_por_sexo($tipo)
+    {
+        //falta capturar y mandar el parametro
+        $year = 2017;
+        $vistaurl = "/reports/report2";
+        $detalle = DB::select("call indicador2(2017)");
+
+        return $this->crearPDF($detalle, $vistaurl, $tipo);
     }
 
-      public function crear_reporte_por_region($tipo){
-      //falta capturar y mandar el parametro
-     $year=2017;
-     $vistaurl="/reports/report3";
-     $detalle=DB::select("call indicador3(2017)");
-     return $this->crearPDF($detalle, $vistaurl,$tipo);
+    public function crear_reporte_por_region($tipo)
+    {
+        //falta capturar y mandar el parametro
+        $year = 2017;
+        $vistaurl = "/reports/report3";
+        $detalle = DB::select("call indicador3(2017)");
+
+        return $this->crearPDF($detalle, $vistaurl, $tipo);
     }
 
-    public function crear_reporte_por_estadia($tipo){
-      //falta capturar y mandar el parametro
-     $year=2017;
-     $vistaurl="/reports/report4";
-     $detalle=DB::select("call indicador4(2017)");
-     return $this->crearPDF($detalle, $vistaurl,$tipo);
+    public function crear_reporte_por_estadia($tipo)
+    {
+        //falta capturar y mandar el parametro
+        $year = 2017;
+        $vistaurl = "/reports/report4";
+        $detalle = DB::select("call indicador4(2017)");
+
+        return $this->crearPDF($detalle, $vistaurl, $tipo);
     }
 
-    public function crear_grafica_porpais(){
+    public function crear_grafica_porpais()
+    {
+        $viewer = DB::select("call indicador1general(2017)");
 
+        $viewer = array_map(function ($viewer) {
+            return (array) $viewer;
+        }, $viewer);
 
-    $viewer =DB::select("call indicador1general(2017)");
-    
-$viewer = array_map(function ($viewer) {
-    return (array)$viewer;
-}, $viewer);
+        $nacionales = array_column($viewer, 'Nacionales');
 
-    $nacionales = array_column($viewer, 'Nacionales');
+        //$click = DB::select("call grafica2(2017)")->get();
+        $extranjeros = array_column($viewer, 'Extranjeros');
 
-    //$click = DB::select("call grafica2(2017)")->get();
-    $extranjeros = array_column($viewer, 'Extranjeros');
-    return view('/reports/report1grafica')->with('viewer',json_encode($extranjeros,JSON_NUMERIC_CHECK))
-    ->with('click',json_encode($nacionales,JSON_NUMERIC_CHECK));
+        return view('/reports/report1grafica')->with('viewer', json_encode($extranjeros, JSON_NUMERIC_CHECK))->with('click', json_encode($nacionales, JSON_NUMERIC_CHECK));
     }
-
-
-
-
-
-
-    
-
 }
