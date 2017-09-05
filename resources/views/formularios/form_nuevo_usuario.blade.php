6000de09cd0f4c8f112b10f97fbbf1038276fff0
Ernesto Herrera <jesh40@hotmail.es>
2017-09-04 21:59:24 -0600

    automatizado

--- resources/views/formularios/form_nuevo_usuario.blade.php
+++ resources/views/formularios/form_nuevo_usuario.blade.php
@@ -160,9 +160,9 @@
             });
         });
         $("#tipo-usuario").on('change', function (e) {
-            //  console.log(e);
-            var id = e.target.value;
-            if (id == 3) {
+            console.log(e);
+            var id = e.target.options[e.target.selectedIndex].text;
+            if (id == 'Hotel' || id == 'hotel') {
                 document.getElementById('text-departamento').style.visibility = 'visible';
                 document.getElementById('text-direc').style.visibility = 'visible';
                 document.getElementById('text-municipio').style.visibility = 'visible';
